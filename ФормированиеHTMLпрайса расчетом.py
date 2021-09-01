36472e4f6141c82d05f90e04cf349ec3d232ac5a
Александр <soroka@energetika.by>
2021-09-01 15:22:49 +0300

    Новый прайс. Новые цены

--- ФормированиеHTMLпрайса расчетом.py
+++ ФормированиеHTMLпрайса расчетом.py
@@ -17,7 +17,7 @@ new_name='Price Energopress TNPA '+den+'-'+mes+'-'+god+'.xls'
 new_nameJ='Price Energopress Journals '+den+'-'+mes+'-'+god+'.xls'
 #path=r'\\Buh\подписка\Прайсы'
 path=''
-price=r'Price Energopress TNPA 20-07-2021.xls'
+price=r'Price Energopress TNPA 01-09-2021.xls'
 
 #Создаем HTML страницу из файла прайса
 
